7176ad6ee9174ab7e2c3b3420370add91bb1a29f
RihoMochizuki <r.mochizuki@rhino-inc.jp>
2025-06-30 13:09:23 +0900

    html、css、js

--- src/js/utils.js
+++ src/js/utils.js
@@ -10,6 +10,45 @@
   switchViewport();
 })();
 
+/* global Splide */
+const setSlider = () => {
+  const splideOptions = {
+    arrows: false,
+    pagination: false,
+    autoWidth: true,
+    type: 'loop',
+    drag: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    autoScroll: {
+      speed: 0.4,
+      pauseOnHover: false,
+    },
+  };
+  const sliderIds = ['#slider1', '#slider2'];
+  sliderIds.forEach((id) => {
+    new Splide(id, splideOptions).mount(window.splide.Extensions);
+  });
+};
+
+const setCTA = () => {
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    const cta = document.querySelector('.l-sticky-cta');
+    const top = document.querySelector('.l-top');
+
+    if (!cta || !top) {
+      return;
+    }
+    const topBottom = top.getBoundingClientRect().bottom + window.scrollY;
+
+    if (window.scrollY >= topBottom) {
+      cta.classList.add('l-sticky-cta--show');
+    } else {
+      cta.classList.remove('l-sticky-cta--show');
+    }
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const observerCallback = (entries) => {
     entries.forEach((entry) => {
@@ -22,8 +61,32 @@ document.addEventListener('DOMContentLoaded', () => {
     threshold: 0.1,
   };
   const defaultObserver = new IntersectionObserver(observerCallback, defaultObserverOptions);
-  const targetElements = document.querySelectorAll('.js-fade, .js-fadeup, .js-fadein');
+  const targetElements = document.querySelectorAll('.js-scrollin');
   targetElements.forEach((target) => {
     defaultObserver.observe(target);
   });
+
+  document.querySelectorAll('a[href^="#"]').forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+  setSlider();
+  setCTA();
+});
+
+window.addEventListener('load', () => {
+  const loadElms = document.querySelectorAll('.js-load');
+  loadElms.forEach((loadElm) => {
+    loadElm.classList.add('js-load--loaded');
+  });
+});
+
+window.addEventListener('scroll', () => {
+  setCTA();
 });
